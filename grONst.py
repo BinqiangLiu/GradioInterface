c618f430733162d1a48b761179cc534ffa8dedfe
BinqiangLiu <134038370+BinqiangLiu@users.noreply.github.com>
2023-07-25 11:52:32 +0800

    Update grONst.py

--- grONst.py
+++ grONst.py
@@ -1,11 +1,14 @@
 import gradio as gr
 import streamlit as st
-
+st.write("---")
 def greet(name):
     return "Hello " + name + "!"
 
 demo = gr.Interface(fn=greet, inputs="text", outputs="text")
     
 demo.launch()   
+st.write("---")
+st.audio(TrackNo56.mp3)
+st.write("---")
+st.audio(TrackNo56.mp3)
 
-st.audio()
